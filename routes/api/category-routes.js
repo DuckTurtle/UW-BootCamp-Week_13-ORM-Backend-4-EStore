76fd2cd54fbba92d9c2c5890721fe78524ecb867
DuckTurtle <qschnell04@gmail.com>
2023-06-05 20:30:15 -0500

    first push

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const cateData = await Category.findAll({
-      inclue: [Product]
+      include: [Product]
     });
     res.status(200).json(cateData);
   } catch (err) {
